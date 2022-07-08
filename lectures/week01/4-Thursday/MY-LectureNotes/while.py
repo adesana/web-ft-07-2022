42adc88050bbc2b8eed7cbf42601c18805b0a6e0
Austin Desana <austindesana@outlook.com>
2022-07-08 09:00:49 -0500

    changes

--- lectures/week01/4-Thursday/MY-LectureNotes/while.py
+++ lectures/week01/4-Thursday/MY-LectureNotes/while.py
@@ -14,3 +14,45 @@
 #4d. If not it prompts the user for the username and password until they get it correct.
 
 #4e. (bonus) have a limited amount of attempts and if they reach the limit it tells the user and exits the program.
+
+
+#1
+i = 1
+while(i<=10):
+    print(i)
+    i+=1
+
+#2
+i = 10
+while(i>=1):
+    print(i)
+    i-=1
+
+#3
+answer = input('Enter a word:   ')
+while answer != 'Stop':
+    answer = input('Enter a word:   ')
+
+
+#4
+
+saved_username = 'adesana'
+saved_password = '1234'
+username = ''
+password = ''
+attempts = 0
+#4b
+
+
+while username != saved_username or password != saved_password:
+    username = input('Enter your username:   ')
+    password = input('Enter your password:   ')
+    attempts+=1
+
+    if attempts > 5:
+        print('You failed, restart the program.')
+        
+    if username == saved_username and password == saved_password:
+        print('Welcome!')
+
+
---
+++ lectures/week01/4-Thursday/homework/thursday_homework.py
@@ -0,0 +1,53 @@
+# Number 1
+
+count1 = 0
+
+list1 = [2, 4, 5]
+list2 = [2, 3, 6]
+list3 = []
+
+while count1 < len(list1):
+    answer = (list1[count1]*list2[count1])
+    count1 +=1
+    list3.append(answer)
+print(list3)
+
+
+
+# Number 2
+
+# index1 = 0
+
+# matrix = [ [2, -2], [5, 3]]
+# list4 = []
+# list5 = []
+# matrix2 = [[1,2],[3,4],]
+# matrix3 = []
+# while index1 < len(matrix):
+#     result = matrix[0][index1] + matrix2[0][index1]
+#     result2 = matrix[1][index1] + matrix2[1][index1]
+#     list4.append(result)
+#     list5.append(result2)
+#     index1 +=1
+# matrix3.append(list4)
+# matrix3.append(list5)
+# print(matrix3)
+
+# Number 3
+
+index1 = 0
+index2 = 0
+index3 = 1
+matrix = [[2, -2],[5, 3],[1,1]]
+list4 = []
+list5 = []
+matrix2 = [[1,2],[3,4],[1,1]]
+matrix3 = []
+while index1 < len(matrix):
+    
+        
+matrix3.append(list4)
+matrix3.append(list5)
+print(matrix3)
+
+
